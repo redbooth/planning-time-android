972e075a4cb302f85a48d34c74e0da62d5bafc9f
Akelael <akelael@gmail.com>
2015-03-23 15:17:56 +0100

    Missing files

---
+++ app/src/main/java/com/redbooth/projectnevada/CardsPagerAdapter.java
@@ -0,0 +1,113 @@
+package com.redbooth.projectnevada;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.redbooth.projectnevada.core.*;
+import com.redbooth.projectnevada.model.CardModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardsPagerAdapter extends FragmentPagerAdapter {
+    private final Dealer dealer;
+    private final List<CardFragment> fragmentList;
+    private final List<CardModel> cardModelList;
+
+    public CardsPagerAdapter(FragmentManager fragmentManager, Dealer dealer) {
+        super(fragmentManager);
+        this.dealer = dealer;
+        this.fragmentList = new ArrayList<>(dealer.getDeckLength());
+        this.cardModelList = new ArrayList<>(dealer.getDeckLength());
+        initializeFragmentPool();
+        initializeCardModelPool();
+    }
+
+    private void initializeFragmentPool() {
+        int count = dealer.getDeckLength();
+        while (count-- > 0) {
+            fragmentList.add(new CardFragment());
+        }
+    }
+
+    private void initializeCardModelPool() {
+        int count = dealer.getDeckLength();
+        while (count-- > 0) {
+            CardModel cardModel = new CardModel();
+            cardModel.setDownwardResourceId(R.drawable.cover_big);
+            cardModel.setUpwardResourceId(R.drawable.card14_big);
+            CardModel.CardStatus cardStatus = CardModel.CardStatus.UPWARDS;
+            if (dealer.getDeckStatus() == Dealer.DeckStatus.DOWNWARDS) {
+                cardStatus = CardModel.CardStatus.DOWNWARDS;
+            }
+            cardModel.setStatus(cardStatus);
+            cardModelList.add(cardModel);
+        }
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        CardFragment fragment = fragmentList.get(position);
+        CardModel cardModel = cardModelList.get(position);
+        cardModel.setUpwardResourceId(getUpwardResourceId(position));
+        fragment.setCard(cardModel);
+        return fragment;
+    }
+
+    private int getUpwardResourceId(int position) {
+        Card card = dealer.getCardAtPosition(position);
+        int upwardResourceId = R.drawable.cover_big;
+        switch (card) {
+            case ONE:
+                upwardResourceId = R.drawable.card01_big;
+                break;
+            case TWO:
+                upwardResourceId = R.drawable.card02_big;
+                break;
+            case THREE:
+                upwardResourceId = R.drawable.card03_big;
+                break;
+            case FIVE:
+                upwardResourceId = R.drawable.card04_big;
+                break;
+            case EIGHT:
+                upwardResourceId = R.drawable.card05_big;
+                break;
+            case THIRTEEN:
+                upwardResourceId = R.drawable.card06_big;
+                break;
+            case TWENTY:
+                upwardResourceId = R.drawable.card07_big;
+                break;
+            case FORTY:
+                upwardResourceId = R.drawable.card08_big;
+                break;
+            case HUNDRED:
+                upwardResourceId = R.drawable.card09_big;
+                break;
+            case INFINITE:
+                upwardResourceId = R.drawable.card10_big;
+                break;
+            case UNKNOWN:
+                upwardResourceId = R.drawable.card11_big;
+                break;
+            case YAK_SHAVING:
+                upwardResourceId = R.drawable.card12_big;
+                break;
+            case BROWN:
+                upwardResourceId = R.drawable.card13_big;
+                break;
+            case PAUSE:
+                upwardResourceId = R.drawable.card14_big;
+                break;
+        }
+        return upwardResourceId;
+    }
+
+    @Override
+    public int getCount() {
+        return dealer.getDeckLength();
+    }
+}
---
+++ core/src/test/java/com/redbooth/projectnevada/core/DealerTest.java
@@ -0,0 +1,99 @@
+package com.redbooth.projectnevada.core;
+
+import com.redbooth.projectnevada.core.Card;
+import com.redbooth.projectnevada.core.Dealer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnit4.class)
+public class DealerTest {
+    private static final int INITIAL_POSITION = 0;
+    private static final Card FIRST_CARD = Card.BROWN;
+    private static final Card SECOND_CARD = Card.UNKNOWN;
+    private static final Card THIRD_CARD = Card.INFINITE;
+    private Dealer dealer;
+    private static final List<Card> cards = new ArrayList<>(3);
+    static {
+        cards.add(FIRST_CARD);
+        cards.add(SECOND_CARD);
+        cards.add(THIRD_CARD);
+    }
+
+    @Before
+    public void setup() {
+       dealer = new Dealer(cards, INITIAL_POSITION);
+    }
+
+    @Test public void shouldReturnTheFirstCard() {
+        Card first = dealer.getCard();
+
+        assertThat(first).isEqualTo(FIRST_CARD);
+    }
+
+    @Test public void shouldReturnSecondCardCallingNextFromFirstCard() {
+        Card secondCard = dealer.getNext();
+
+        assertThat(secondCard).isEqualTo(SECOND_CARD);
+    }
+
+    @Test public void shouldReturnFirstCardCallingNextFromLastCard() {
+        dealer.moveNext();
+        dealer.moveNext();
+        Card firstCard = dealer.getNext();
+
+        assertThat(firstCard).isEqualTo(FIRST_CARD);
+    }
+
+    @Test public void shouldReturnLastCardCallingPreviousFromFirstCard() {
+        Card lastCard = dealer.getPrevious();
+
+        assertThat(lastCard).isEqualTo(THIRD_CARD);
+    }
+
+    @Test public void shouldReturnTrueIfIsLastCard() {
+        dealer.movePrevious();
+        boolean isLast = dealer.isLast();
+
+        assertThat(isLast).isEqualTo(true);
+    }
+
+    @Test public void shouldReturnFalseIfNotLastCard() {
+        dealer.getNext();
+        boolean isLast = dealer.isLast();
+
+        assertThat(isLast).isEqualTo(false);
+    }
+
+    @Test public void shouldReturnDeckLength() {
+        int length = dealer.getDeckLength();
+
+        assertThat(length).isEqualTo(3);
+    }
+
+    @Test public void shouldBeInitializedUpwards() {
+        Dealer.DeckStatus status = dealer.getDeckStatus();
+
+        assertThat(status).isEqualTo(Dealer.DeckStatus.UPWARDS);
+    }
+
+    @Test public void shouldBeDownwardsAfterFirstFlip() {
+        dealer.flipDeck();
+
+        assertThat(dealer.getDeckStatus()).isEqualTo(Dealer.DeckStatus.DOWNWARDS);
+    }
+
+    @Test public void shouldBeUpwardsAfterAFlipFromDownwards() {
+        dealer.flipDeck();
+        dealer.flipDeck();
+
+        assertThat(dealer.getDeckStatus()).isEqualTo(Dealer.DeckStatus.UPWARDS);
+    }
+}
