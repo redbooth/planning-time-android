d90c61cfa41efaf2f7c3fc85e79b755951066fa6
Akelael <akelael@gmail.com>
2015-03-29 21:26:38 +0200

    Fixed bug with fragments in pager

--- app/src/main/java/com/redbooth/projectnevada/MainActivity.java
+++ app/src/main/java/com/redbooth/projectnevada/MainActivity.java
@@ -2,8 +2,11 @@ package com.redbooth.projectnevada;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class MainActivity extends FragmentActivity {
+    private static final String TAG_CARD_LIST_FRAGMENT = "CardListFragment";
+    private static final String TAG_CARD_GRID_FRAGMENT = "CardGridFragment";
     private CardListFragment cardListFragment;
     private CardGridFragment cardGridFragment;
 
@@ -11,15 +14,38 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cardListFragment = CardListFragment.newInstance();
-        cardGridFragment = CardGridFragment.newInstance();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.content_layout, cardListFragment, "CardListFragment")
-                .add(R.id.content_layout, cardGridFragment, "CardGridFragment")
-                .hide(cardGridFragment)
-                .show(cardListFragment)
-                .commit();
+        initializeFragments();
+    }
+
+    private void initializeFragments() {
+        initializeGridFragment();
+        initializeListFragment();
+    }
+
+    private void initializeListFragment() {
+        cardListFragment = (CardListFragment)
+                getSupportFragmentManager().findFragmentByTag(TAG_CARD_LIST_FRAGMENT);
+        if (cardListFragment == null) {
+            cardListFragment = CardListFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.content_layout, cardListFragment, TAG_CARD_LIST_FRAGMENT)
+                    .show(cardListFragment)
+                    .commit();
+        }
+    }
+
+    private void initializeGridFragment() {
+        cardGridFragment = (CardGridFragment)
+                getSupportFragmentManager().findFragmentByTag(TAG_CARD_GRID_FRAGMENT);
+        if (cardGridFragment == null) {
+            cardGridFragment = CardGridFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.content_layout, cardGridFragment, TAG_CARD_GRID_FRAGMENT)
+                    .hide(cardGridFragment)
+                    .commit();
+        }
     }
 
     public void selectCard(int position) {
