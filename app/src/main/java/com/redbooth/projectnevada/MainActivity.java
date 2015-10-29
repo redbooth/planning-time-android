a0c29db759a0f1d52b30db46968c0a8c927c5857
Akelael <akelael@gmail.com>
2015-10-29 09:32:29 +0100

    Cleaning build.gradle

--- app/src/main/java/com/redbooth/projectnevada/MainActivity.java
+++ app/src/main/java/com/redbooth/projectnevada/MainActivity.java
@@ -2,7 +2,6 @@ package com.redbooth.projectnevada;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
 public class MainActivity extends FragmentActivity {
     private static final String TAG_CARD_LIST_FRAGMENT = "CardListFragment";
